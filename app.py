f9fc88fe4de03a3174707994bf5b00d588c6acdb
hermeskongo <hermeskongo@gmail.com>
2026-01-28 07:00:51 +0100

    feat: parsing et stockage des données reçu depuis arduino

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import serial
 import time
 import threading
+import json
 
 try:
     ser = serial.Serial('COM1', 9600, timeout=1)
@@ -9,5 +10,27 @@ try:
 except Exception as e:
     print(f"Connection failed: ${e}")
 
+
+data = {
+    'temperature': None,
+    'humidity': None,
+    'pressure': None
+}
+
+# T:22.00 H:47.00 P:1080.00
+def read_arduino():
+    while True:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8', errors='ignore').strip()
+            
+            for part in line.split(' '):
+                if(part.startswith('T')):
+                    data['temperature'] = float(part.split(':')[1].strip())
+                    
+                elif(part.startswith('H')):
+                    data['humidity'] = float(part.split(':')[1].strip())
+                    
+                elif(part.startswith('P')):
+                    data['pressure'] = float(part.split(':')[1].strip())
 
 
