c29594588ce2d38d8536f0fbe8502190117d88ed
hermeskongo <hermeskongo@gmail.com>
2026-02-15 13:20:50 +0000

    feat: amélioration de la gestion de la connexion série et ajout de la fonctionnalité de contrôle du ventilateur via le protocole WebSocket

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import serial
 import time
 import threading
-import json
+
+from flask_socketio import SocketIO
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 try:
-    ser = serial.Serial('COM1', 9600, timeout=1)
+    # Ouvrez d'abord sans spécifier le baudrate
+    ser = serial.Serial()
+    ser.port = 'COM1'
+    ser.baudrate = 9600
+    ser.bytesize = serial.EIGHTBITS
+    ser.parity = serial.PARITY_NONE
+    ser.stopbits = serial.STOPBITS_ONE
+    ser.timeout = 1
+
+    # Fermez si déjà ouvert, puis réouvrez
+    if ser.is_open:
+        ser.close()
+    time.sleep(0.5)
+
+    ser.open()
+    time.sleep(1)
     print("Connection successfully !✅ ")
     time.sleep(.2)
 except Exception as e:
@@ -32,24 +52,27 @@ def read_arduino():
                     
                 elif(part.startswith('P')):
                     data['pressure'] = float(part.split(':')[1].strip())
-
-# Démarrage du thread de lecture ppour récupérer les données en continue en arrière-plan
-reader_thread = threading.Thread(target=read_arduino, daemon=True)
-reader_thread.start()
+            
+            socketio.emit('sensor_update', data)
+        time.sleep(3)
    
 
 def send_command(command):
-    ser.write(f"{command}\n".encode('utf-8'))
+    ser.write(f"{command.upper()}\n".encode('utf-8'))
     print(f"Sent command: {command}")
 
 
-def on():
-    """Fonction qui permet d'alumer le ventilateur Arduino"""
-    send_command("ON")
-    
-def off():
-    """Fonction qui permet d'éteindre le ventilateur Arduino"""
-    send_command("OFF")
+@socketio.on('toggle_fan')
+def toggleFan(data):
+    """Permet de d'allumer ou d'éteindre le ventilateur une action reçue du client.
+
+    Args:
+        data (dict): Dictionnaire contenant 'command' ('ON' ou 'OFF')
+    """
+    command = data.get('action')
+    send_command(command)
+
+
     
     
 def show_data():
@@ -58,39 +81,19 @@ def show_data():
         print(f"{key}: {value}")
 
 
-if(__name__ == 'main'):
-    def show_menu():
-        print("\n━━━━━━━━━━━━━━━━━━━━━━━━━━")
-        print("Commandes disponibles:")
-        print("  1 - Allumer ventilateur")
-        print("  2 - Éteindre ventilateur")
-        print("  3 - Afficher dernières données")
-        print("  q - Quitter")
-        print("━━━━━━━━━━━━━━━━━━━━━━━━━━\n")
+@app.route('/get_data')
+def hello_world():
+    return jsonify(data)
 
 
-    show_menu()
+@app.route('/bob')
+def bob():
+    return jsonify(message="Hello BOB", status="success")
 
-    try:
-        while True:
-            command = input("> ").strip()
-            
-            if(command == '1'):
-                on()
-            elif command == '2':
-                off()
-            elif command == '3':
-                show_data()
-            elif command == 'q':
-                print("👋 Fermeture...")
-                ser.close()
-                break
-            else:
-                print("❌ Commande invalide")
-                show_menu()
-                
-    except KeyboardInterrupt:
-        print("Exciting by Keyboard interruption")
-        exit()
-        
 
+if __name__ == '__main__':
+    port = 8000
+    reader_thread = threading.Thread(target=read_arduino, daemon=True)
+    reader_thread.start()
+    print(f"Server launched on port: {port}")
+    socketio.run(app, port=port, debug=False, allow_unsafe_werkzeug=False)
